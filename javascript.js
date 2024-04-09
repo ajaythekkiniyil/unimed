cb45d5895306c57d5ba46bfbc16cc7cd1d5869ab
ajay-equipo <ajay.t@equipohealth.com>
2024-04-09 22:58:07 +0530

    footer and slider improvements

--- javascript.js
+++ javascript.js
@@ -5,3 +5,18 @@ function openNav() {
 function closeNav() {
   document.getElementById("myNav").style.height = "0%";
 }
+
+$('.slick-slider').slick({
+  slidesToShow: 3,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1
+      }
+    }
+  ]
+});
